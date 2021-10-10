f5add1fcdc6c35555ad084a474bd861d49f110b2
ildadu <litvilyanot300@mail.ru>
2021-10-10 15:50:54 +0300

    Йа Добавил Новый Проект

---
+++ 2021.10.03 Mnogoetazhki.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Заданная Квартира = ";
+	int n = 0;
+	cin >> n;
+	int p = (n - 1)/36 + 1;
+	cout << "Подъезд = " << p << endl;
+	int e = (n - 1) / 4 % 9 + 1;
+	cout << "Этаж = " << e << endl;
+
+	system("pause");
+	return 0;
+}
