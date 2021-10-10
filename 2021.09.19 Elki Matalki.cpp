c554f39b46de5cd39147d64086eb493ce1175c30
ildadu <litvilyanot300@mail.ru>
2021-10-10 15:59:17 +0300

    НичоНеДобавили

---
+++ 2021.09.19 Elki Matalki.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char a = '8';
+	char b = '#';
+	cout << "   " << a << endl;
+	cout << "  " << a << a << a << endl;
+	cout << " " << a << a << a << a << a << endl;
+	cout << "   " << b << endl;
+	cout << "   " << b << endl;
+	system("pause");
+	return 0;
+}
